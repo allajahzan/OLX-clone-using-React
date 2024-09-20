eaa9e08343390198815300c3f8d5ccaa8c01ce60
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-09-20 10:03:23 +0530

    changed detail page style

--- src/pages/itemDetails/itemDetails.tsx
+++ src/pages/itemDetails/itemDetails.tsx
@@ -2,48 +2,47 @@ import { useState } from "react"
 import Navbar from "../../components/navbar/navbar"
 import LoginModal from "../../components/modal/loginModal"
 import SellItemModal from "../../components/modal/sellItemModal"
-import { itemsContext , contextType} from "../../context/itemsContext"
+import { itemsContext, contextType } from "../../context/itemsContext"
 import { useLocation } from "react-router-dom"
 
 function ItemDetails() {
 
-    let [openModal, setModal] = useState<boolean>(false)
-    let [openModalSell, setModalSell] = useState<boolean>(false)
-    let itemContext : contextType | null = itemsContext()
-    
-    let toggleModal = () => {
-        setModal(!openModal)
-    }
+  let [openModal, setModal] = useState<boolean>(false)
+  let [openModalSell, setModalSell] = useState<boolean>(false)
+  let itemContext: contextType | null = itemsContext()
 
-    let toggleModalSell = () => {
-        setModalSell(!openModalSell)
-    }
+  let toggleModal = () => {
+    setModal(!openModal)
+  }
 
-    let location = useLocation()
+  let toggleModalSell = () => {
+    setModalSell(!openModalSell)
+  }
+
+  let location = useLocation()
 
   return (
     <div>
-       <Navbar toggleModalSell={toggleModalSell} toggleModal={toggleModal} />
-       <LoginModal toggleModal={toggleModal} status={openModal} />
+      <Navbar toggleModalSell={toggleModalSell} toggleModal={toggleModal} />
+      <LoginModal toggleModal={toggleModal} status={openModal} />
 
-       <div className="grid gap-5 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 p-10 px-5 sm:px-15 md:px-30 lg:px-40">
-            <div className="border-2 w-full rounded-lg flex justify-center overflow-hidden h-96">
-              <img className="w-full object-contain" src={location.state.item.imageUrl} alt="" />
-            </div>
-            <div className="flex flex-col relative">
-              <div className=" w-full bottom-0 flex justify-between pb-3">
-              <p className="p-1 pl-0 font-bold">{location.state.item.userName}</p>
-              <p className="p-1 pl-0 text-sm">{location.state.item.createdAt}</p>
-              </div>
-              <p className="p-1 pl-0 text-xl">{location.state.item.title}</p>
-              <p className="p-1 pl-0">{location.state.item.category}</p>
-              <p className="p-1 pl-0">₹ {location.state.item.price}</p>
-              <p className="p-1 pl-0 text-ellipsis overflow-hidden">{location.state.item.description}</p>
-              
-            </div>
-       </div>
+      <div className="grid gap-5 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 p-10 px-5 sm:px-15 md:px-30 lg:px-40">
+        <div className="border-2 w-full rounded-lg flex justify-center overflow-hidden h-96">
+          <img className="object-cover" src={location.state.item.imageUrl} alt="" />
+        </div>
+        <div className="flex flex-col relative">
+          <p className="p-1 pl-0 text-2xl font-bold">₹ {location.state.item.price}</p>
+          <p className="p-1 pl-0 text-base">{location.state.item.category}</p>
+          <p className="p-1 pl-0 text-xl font-bold">{location.state.item.title}</p>
+          <p className="p-1 pl-0 text-ellipsis overflow-hidden">{location.state.item.description}</p>
+          <div className="absolute w-full bottom-0 flex justify-between pb-3">
+            <p className="p-1 pl-0 font-bold">{location.state.item.userName}</p>
+            <p className="p-1 pl-0 text-sm">{location.state.item.createdAt}</p>
+          </div>
+        </div>
+      </div>
 
-       <SellItemModal setItems={(itemContext as contextType).setItems} toggleModal={toggleModalSell} status={openModalSell} />
+      <SellItemModal setItems={(itemContext as contextType).setItems} toggleModal={toggleModalSell} status={openModalSell} />
     </div>
   )
 }
